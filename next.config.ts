605a18a6562c55383057567677ecf83fe468b5f2
eleanor <eleanor128@gmail.com>
2026-01-14 17:57:35 +0800

    Fix basePath for GitHub User Page

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === 'production' ? '/MyWebsite' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/MyWebsite' : '',
   images: {
     unoptimized: true, 
   },
